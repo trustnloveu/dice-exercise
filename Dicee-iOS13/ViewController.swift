f06d6569c0021ddb3e3d2ba30911a6b986a3af85
ejyang <trustnloveu@gmail.com>
2021-08-18 00:29:21 +0900

    Set simple button event

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -1,22 +1,34 @@
-//
-//  ViewController.swift
-//  Dicee-iOS13
-//
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
+// IBOutlet goes from Code to Design
+// IBAction goes from Code to Design
 
 import UIKit
 
+// Controller
 class ViewController: UIViewController {
+    
+    // first dice on left
     @IBOutlet weak var diceImageView1: UIImageView!
     
+    // second dice on right
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+
+    // viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView1.alpha = 0.5
         
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
-
+    // button handler
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Button got tapped.")
+        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    }
 }
 
